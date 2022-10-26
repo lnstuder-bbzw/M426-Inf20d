7f9da7e6e4bfc1bb2c263ac0be58d606fb4861d9
Lian Studer <ln.studer@protonmail.ch>
2022-10-26 09:26:31 +0200

    feat(statistics): add median method

--- Aufgaben/01-unittest/Statistics/Average.cs
+++ Aufgaben/01-unittest/Statistics/Average.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Statistics
 {
@@ -7,6 +8,9 @@ namespace Statistics
     {
         public double Mean(List<int> numbers)
         {
+            if (numbers.Count() == 0)
+                throw new ArgumentException("List of numbers is empty");
+
             int sum = 0;
             double mean = 0.0;
 
@@ -18,5 +22,25 @@ namespace Statistics
             mean = (double)sum / numbers.Count;
             return mean;
         }
+
+        public float Median(List<int> numbers)
+        {
+            numbers.Sort();
+            int count = numbers.Count();
+
+            if (count == 0)
+                throw new ArgumentException("List of numbers is empty");
+
+            if (count % 2 != 0)
+            {
+                int medianIndex = (count - 1) / 2;
+                return numbers[medianIndex];
+            }
+
+            int pivot = count / 2;
+            return (numbers[pivot] + numbers[pivot - 1]) / 2;
+        }
     }
 }
+
+// { 2, 2, 3, 3, 5, 8, 9 }
