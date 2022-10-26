aab6e834c8b66eb7736922e5ca8f40ce49be1e4c
Lian Studer <ln.studer@protonmail.ch>
2022-10-26 09:26:10 +0200

    test(statistics): add tests for mean and median

--- Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
+++ Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
@@ -9,13 +9,67 @@ namespace Statistics.Tests
         [Fact]
         public void TestMeanOfTwoNumbers()
         {
-            List<int> numbers = new List<int> { 1, 3 };
-            double expected = 2;
+            List<int> numbers = new List<int> { 20, 30 };
+            double expected = 25;
             Average average = new Average();
 
             double actual = average.Mean(numbers);
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMeanOfFiveNumbers()
+        {
+            List<int> numbers = new List<int> { 20, 30, 7, 40, 60 };
+            double expected = 31.4;
+            Average average = new Average();
+
+            double actual = average.Mean(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMeanOfNoNumbers()
+        {
+            List<int> numbers = new List<int>();
+            Average average = new Average();
+
+            Assert.Throws<ArgumentException>(() => average.Mean(numbers));
+        }
+
+        [Fact]
+        public void TestMedianForOddNumberOfElements()
+        {
+            List<int> numbers = new List<int> { 3, 5, 2, 8, 9, 2, 3 };
+            float expected = 3;
+            Average average = new Average();
+
+            float actual = average.Median(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMedianForEvenNumberOfElements()
+        {
+            List<int> numbers = new List<int> { 3, 5, 2, 9, 2, 3 };
+            float expected = 3;
+            Average average = new Average();
+
+            float actual = average.Median(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestMedianOfNoNumbers()
+        {
+            List<int> numbers = new List<int>();
+            Average average = new Average();
+
+            Assert.Throws<ArgumentException>(() => average.Median(numbers));
         }
     }
 }
