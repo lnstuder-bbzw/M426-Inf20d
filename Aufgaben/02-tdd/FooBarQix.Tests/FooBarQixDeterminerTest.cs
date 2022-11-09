41917ad98ee8c6cb8a6e12f542ea5033a10bb8f3
Lian Studer <ln.studer@protonmail.ch>
2022-11-09 08:17:10 +0100

    test: basic tests

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -5,8 +5,50 @@ namespace FooBarQix.Tests
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
+        public void NotDividableByThreeOrFiveOrSevenIsArg()
         {
+            var determiner = new FooBarQixDeterminer();
+            int arg = 4;
+
+            string result = determiner.Determine(arg);
+
+            Assert.Equal(result, arg.ToString());
+        }
+
+        [Fact]
+        public void DividableByThreeIsFoo()
+        {
+            var determiner = new FooBarQixDeterminer();
+            int arg = 9;
+            string expected = "Foo";
+
+            string result = determiner.Determine(arg);
+
+            Assert.Equal(result, expected);
+        }
+
+        [Fact]
+        public void DividableByFiveIsBar()
+        {
+            var determiner = new FooBarQixDeterminer();
+            int arg = 10;
+            string expected = "Bar";
+
+            string result = determiner.Determine(arg);
+
+            Assert.Equal(result, expected);
+        }
+
+        [Fact]
+        public void DividableBySevenIsQix()
+        {
+            var determiner = new FooBarQixDeterminer();
+            int arg = 14;
+            string expected = "Qix";
+
+            string result = determiner.Determine(arg);
+
+            Assert.Equal(result, expected);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -2,6 +2,21 @@
 {
     public class FooBarQixDeterminer
     {
-
+        public string Determine(int num)
+        {
+            if (num % 3 == 0)
+            {
+                return "Foo";
+            }
+            else if (num % 5 == 0)
+            {
+                return "Bar";
+            }
+            else if (num % 7 == 0)
+            {
+                return "Qix";
+            }
+            return num.ToString();
+        }
     }
 }
